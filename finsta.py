ea97b6179678547f9f37bb2c46f293df44343e4b
Gabriel Krell <gabrielkrell@users.noreply.github.com>
2018-03-29 03:25:05 -0500

    Include stderr, stfout in change_hostname debug
    For later

--- finsta.py
+++ finsta.py
@@ -129,9 +129,14 @@ def update_hostname():
 
     try:
         subprocess.run(
-            ['sudo', '/usr/sbin/change_hostname.sh', hostname])
+            ['sudo', '/usr/sbin/change_hostname.sh', hostname],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
     except subprocess.CalledProcessError as e:
-        return jsonify({'returncode': e.returncode}), 500
+        return jsonify({'returncode': e.returncode,
+                        'stdout': e.stdout,
+                        'stderr': e.stderr
+                        }), 500
     else:
         return 'OK', 200
 
