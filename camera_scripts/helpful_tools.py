735ced64555f7f747c18a894f91b6d12ce109530
Gabriel Krell <gabrielkrell@users.noreply.github.com>
2018-06-17 08:27:42 +0530

    Split take_picture into two files
    Can't test this until we have an actual Pi around.

---
+++ camera_scripts/helpful_tools.py
@@ -0,0 +1,42 @@
+"""
+This file contains tools to make your camera scripts easier. The example runs
+code from this file.  You shouldn't need to edit this or make your own copy.
+"""
+from picamera import PiCamera, PiCameraMMALError
+from time import sleep
+import os
+from contextlib import contextmanager
+
+
+def make_image_path(filename):
+    """Returns the complete path to a new image with the given filename.
+    """
+    scripts_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(
+        scripts_dir, os.path.pardir, 'static', 'images', filename)
+
+
+@contextmanager
+def shielded_camera():
+    """Context manager: handle PiCamera opening/closing and retry logic.
+
+    Open a PiCamera in our standard resolution, retrying if it's in use.  After
+    use, clean up with camera.close().
+    :returns: a PiCamera instance
+    :rtype: PiCamera
+    """
+    while True:
+        try:
+            camera = PiCamera(resolution=(600, 600))
+            try:
+                yield camera
+            finally:
+                camera.close()
+            break
+        except PiCameraMMALError:
+            print('Someone else is using the camera. Retrying...')
+            sleep(1)
+
+
+if __name__ == "__main__":
+    print("This script doesn't do anything by itself.  Try take_picture.py instead.")
--- camera_scripts/take_picture.py
+++ camera_scripts/take_picture.py
@@ -1,29 +1,8 @@
-from picamera import PiCamera, Color, PiCameraMMALError
+from helpful_tools import shielded_camera, make_image_path
+
+from picamera import Color
 from time import sleep
 import datetime as dt
-import os
-
-
-def get_full_image_name(filename):
-    """Returns the path to a new image with the given filename.
-       You shouldn't change this function when making your own script.
-    """
-    scripts_dir = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(
-        scripts_dir, os.path.pardir, 'static', 'images', filename)
-
-
-def start_camera():
-    """Starts up the camera, retrying if it's currently in use.
-       You shouldn't change this function when making your own script.
-    """
-    while True:
-        try:
-            return PiCamera(resolution=(600, 600))
-            break
-        except PiCameraMMALError:
-            print('Someone else is using the camera. Retrying...')
-            sleep(1)
 
 
 def click():
@@ -33,8 +12,7 @@ def click():
     Run it from the terminal with
         python3 this_script.py
     """
-    try:
-        camera = start_camera()
+    with shielded_camera() as camera:
         camera.annotate_background = Color('pink')
         camera.annotate_foreground = Color('blue')
         camera.annotate_text_size = 30
@@ -47,11 +25,8 @@ def click():
         # saturation, colorswap, washedout, posterise, colorpoint, cartoon
 
         sleep(2)  # wait for the camera to be ready
-        camera.capture(get_full_image_name('img_%s.jpg' % date_str))
+        camera.capture(make_image_path('img_%s.jpg' % date_str))
         print('You took a photo!')
-    finally:
-        # even if picture fails, make sure the next person can use the camera
-        camera.close()
 
 
 if __name__ == "__main__":
